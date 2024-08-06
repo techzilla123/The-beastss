429fec8d5a282e7038a0fe523407b0c97dea2a36
Monsur Raji <monsurrajiybs@gmail.com>
2024-08-06 02:55:36 +0100

    ss

--- components/anime/player/VideoPlayer.jsx
+++ components/anime/player/VideoPlayer.jsx
@@ -7,7 +7,6 @@ import { defaultLayoutIcons, DefaultVideoLayout, DefaultAudioLayout } from '@vid
 
 // Constants
 const corsLink = process.env.NEXT_PUBLIC_CORS_REQUEST_LINK || "http://localhost:8080"; // Use updated CORS Anywhere proxy port
-// Use local CORS Anywhere proxy
 
 const VideoPlayer = ({ episodeTitle, episodeName, episodeNumber, onVideoLoad }) => {
   const [episodeDataLink, setEpisodeDataLink] = useState(null);
